1017a012027fd48a9d599a9bbaed60681dcedead
Fernanda9421 <fer.candrade21@gmail.com>
2021-07-05 10:54:05 -0300

    Exemplos de Operadores - Aulas JavaScript Básico

---
+++ script.js
@@ -0,0 +1,87 @@
+// // Operadores Aritméticos:
+
+// // Soma +
+// var sumTwoNumbers = 3+1;
+// console.log(sumTwoNumbers);
+
+// // Subtração -
+// var subTwoNumbers = 4-2;
+// console.log(subTwoNumbers);
+
+// // Multiplicação *
+// var mulTwoNumbers = 3*4;
+// console.log(mulTwoNumbers);
+
+// // Divisão /
+// var divTwoNumbers = 4/2;
+// console.log(divTwoNumbers);
+
+// // Módulo ou resto %
+// console.log(8%3);
+
+// // Incremento ++
+// var num = 2;
+// console.log(++num);
+// num = num + 1;
+
+// // Decremento --
+// var num = 2;
+// console.log(--num);
+// num = num - 1;
+
+// Operadores de Atribuição
+
+//Atribuição de soma
+//var x = 5;
+//var y = 1;
+//console.log(x+=y); //retorna 6 (ou seja, x = x+y)
+
+//Atribuição de subtração
+//console.log(x-=y); //retorna 4
+
+//Atribuição de multiplicação
+//console.log(x*=y); //retorna 5
+
+//Atribuição de divisão
+//console.log(x/=y); //retorna 5
+
+//Atribuição de Módulo
+//console.log(x%=y); //retorna 0
+
+//Operadores de Comparação
+// x = 5;
+// y = 2;
+
+// == compara se um valor é igual ao outro
+//console.log(x==y); //retorma false
+
+// === compara se o valor e o tipo são iguais
+//console.log(x===y); // retorna false (o tipo é igual, mas os valores não)
+
+// != compara se um valor é diferente do outro
+//console.log(x!=y); // retorna true
+
+// Menor que <
+//console.log(x<y); //retorna false
+
+//Menor ou igual que <=
+//console.log(x<=y); //retorna false
+
+//Maior que >
+//console.log(x>y); //retorna true
+
+//Maior ou igual que >=
+//console.log(x>=y); //retorna true
+
+//Operadores Lógicos
+// x = 5;
+// y = 3;
+// z = 5;
+
+// E &&
+//console.log(x==y && x==z); //retorna false
+
+//OU ||
+//console.log(x==y || x==z); //retorna true
+
+// NEGAÇÃO !
